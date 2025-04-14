a33d0020df148c869688715f93889b1f34548789
Hallgrimur Thorsteinsson <hat@icepower.dk>
2025-04-14 17:17:34 +0200

    Fixing my BRANCH

---
+++ unity/VR_Final/Assets/FireBulletOnActivate.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePoint;
+    public float bulletSpeed = 20f;
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
+
+    void Awake()
+    {
+        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        grabInteractable.activated.AddListener(FireBullet);
+    }
+
+    void FireBullet(ActivateEventArgs args)
+    {
+        GameObject spawnedBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().linearVelocity = firePoint.forward * bulletSpeed;
+        Destroy(spawnedBullet, 5f);
+    }
+}
