560cca08584fb9a75e827b15b8dec3eddbc775b4
Hallgrimur Thorsteinsson <hat@icepower.dk>
2025-05-08 15:59:51 +0200

    fixed spawner

--- unity/VR_Final/Assets/scripts/CowboySpawner.cs
+++ unity/VR_Final/Assets/scripts/CowboySpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class CowboySpawner : MonoBehaviour
@@ -6,25 +7,43 @@ public class CowboySpawner : MonoBehaviour
     [SerializeField] private GameObject cowboyPrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private int maxCowboys = 8;
+    [SerializeField] private float spawnRadiusX = 5f;
+    [SerializeField] private float spawnRadiusZ = 5f;
+
+    // …
+
+
 
     private List<GameObject> activeCowboys = new List<GameObject>();
 
     void Start()
     {
-        SpawnInitialCowboys();
+        // Start the coroutine to spawn cowboys with a delay
+        StartCoroutine(SpawnInitialCowboys());
     }
 
-    void SpawnInitialCowboys()
+    private IEnumerator SpawnInitialCowboys()
     {
         for (int i = 0; i < maxCowboys; i++)
         {
             SpawnCowboy();
+            // Wait for 1 second before spawning the next cowboy
+            yield return new WaitForSeconds(1f);
         }
     }
 
     void SpawnCowboy()
     {
-        GameObject cowboy = Instantiate(cowboyPrefab, spawnPoint.position, spawnPoint.rotation);
+        // Instantiate the cowboy at a random position around the spawner
+        
+        Vector3 randomOffset = new Vector3(
+            Random.Range(-spawnRadiusX, spawnRadiusX),
+            0f,
+            Random.Range(-spawnRadiusZ, spawnRadiusZ)
+        );
+        Vector3 spawnPosition = spawnPoint.position + randomOffset;
+
+        GameObject cowboy = Instantiate(cowboyPrefab, spawnPosition, spawnPoint.rotation);
         Cowboy cowboyScript = cowboy.GetComponent<Cowboy>();
         cowboyScript.spawner = this; // Link back for callback on death
         activeCowboys.Add(cowboy);
--- unity/VR_Final/Assets/scripts/cowboy.cs
+++ unity/VR_Final/Assets/scripts/cowboy.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using System.Collections;
@@ -12,6 +12,7 @@ public class Cowboy : MonoBehaviour, IHittable
         Running,
         Ragdoll
     }
+
     private Rigidbody[] rigidbodies;
     private Animator animator;
     [SerializeField] private Transform hatTransform;
@@ -20,16 +21,14 @@ public class Cowboy : MonoBehaviour, IHittable
     private CharacterController controller;
     [SerializeField] private Camera camera;
     private State currentState = State.Walking;
-    public CowboySpawner spawner; // public so it can be assigned at runtime
-
+    public CowboySpawner spawner;
 
     void Awake()
     {
         Debug.Log("Cowboy spawned!");
-
         rigidbodies = GetComponentsInChildren<Rigidbody>();
         animator = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>(); // <-- add this
+        controller = GetComponent<CharacterController>();
         DisableRagdoll();
     }
 
@@ -42,7 +41,6 @@ public class Cowboy : MonoBehaviour, IHittable
     public void EnableRagdoll()
     {
         animator.enabled = false;
-
         foreach (var rb in rigidbodies)
         {
             rb.isKinematic = false;
@@ -50,14 +48,12 @@ public class Cowboy : MonoBehaviour, IHittable
             rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
 
-        // Unparent the hat
         if (hatTransform != null)
         {
             hatTransform.SetParent(null);
             hatRigidbody.isKinematic = false;
             hatRigidbody.useGravity = true;
             hatRigidbody.AddForce(Vector3.up * 1.5f + Vector3.back * 2f, ForceMode.Impulse);
-            // Add a torque to the hat
             hatRigidbody.AddTorque(Vector3.up * 10f, ForceMode.Impulse);
         }
 
@@ -66,7 +62,7 @@ public class Cowboy : MonoBehaviour, IHittable
 
     private IEnumerator NotifyDeathCoroutine()
     {
-        yield return new WaitForSeconds(3f); // Let the ragdoll play out
+        yield return new WaitForSeconds(3f);
         spawner?.OnCowboyDied(gameObject);
     }
 
@@ -74,67 +70,44 @@ public class Cowboy : MonoBehaviour, IHittable
     {
         foreach (var rb in rigidbodies)
         {
-            //disable hitbox
             var col = rb.GetComponent<Collider>();
-            if (col != null)
-            {
-                col.enabled = true;
-            }
+            if (col != null) col.enabled = true;
             rb.isKinematic = true;
-
-            
-            //gravity is on 
         }
 
         if (animator != null) animator.enabled = true;
     }
 
-    //void WalkingBehavior()
-    //{
-    //    Vector3 direction = camera.transform.position - transform.position;
-    //    direction.y = 0;
-    //    direction.Normalize();
-
-    //    Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 20 * Time.deltaTime);
-
-    //    Vector3 moveVector = transform.forward * walkSpeed;
-
-    //    //transform.position += transform.forward * 1.5f * Time.deltaTime;
-    //    controller.Move(moveVector * Time.deltaTime);
-
-
-    //}
-
-    void RunningBehavior()
-    {
-        if (currentState != State.Running)
-        {
-            currentState = State.Running;
-            animator.SetBool("isRunning", true);
-        }
-    }
-
     void WalkingBehavior()
     {
-        Vector3 direction = camera.transform.position - transform.position;
-        direction.y = 0;
-        direction.Normalize();
+        animator.SetBool("isWalking", true);
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
 
-        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 20 * Time.deltaTime);
+        // then your usual check...
+        if (!animator.IsInTransition(0)
+            && state.IsName("Walking")
+            && state.normalizedTime >= 0.1f)
+        {
 
-        Vector3 moveVector = transform.forward * walkSpeed;
-        controller.Move(moveVector * Time.deltaTime);
+            //log the state
+            Vector3 direction = camera.transform.position - transform.position;
+            direction.y = 0;
+            direction.Normalize();
 
-        animator.SetBool("isWalking", true); // Animate walking
+            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 20 * Time.deltaTime);
+
+            Vector3 moveVector = transform.forward * walkSpeed;
+            controller.Move(moveVector * Time.deltaTime);
+            
+        }
     }
+    
 
     void IdleBehavior()
     {
-        animator.SetBool("isWalking", false); // Animate idle
+        animator.SetBool("isWalking", false);
     }
-
 
     void RagdollBehavior()
     {
@@ -148,20 +121,10 @@ public class Cowboy : MonoBehaviour, IHittable
 
     void Update()
     {
-        float distanceToCamera = Vector3.Distance(transform.position, camera.transform.position);
-
         if (currentState != State.Ragdoll)
         {
-            if (distanceToCamera > 2.5f)
-            {
-                currentState = State.Walking;
-            }
-            else
-            {
-                currentState = State.Idle;
-                Debug.Log("Current State: " + currentState);
-
-            }
+            float distanceToCamera = Vector3.Distance(transform.position, camera.transform.position);
+            currentState = distanceToCamera > 2.5f ? State.Walking : State.Idle;
         }
 
         switch (currentState)
@@ -172,14 +135,10 @@ public class Cowboy : MonoBehaviour, IHittable
             case State.Walking:
                 WalkingBehavior();
                 break;
-            case State.Running:
-                RunningBehavior();
-                break;
             case State.Ragdoll:
                 RagdollBehavior();
                 break;
         }
-
     }
-
 }
+
